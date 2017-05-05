dcdbc9bbc5944b8dc8d00fd6b7d639d70ff34fcd
ballsaii <g.ballsaii@gmail.com>
2017-05-05 17:23:57 +0700

    Complete 2 basic plots
    - fake legend for stairs display is done
    - use matlab structure to render

--- transverse_profile.m
+++ transverse_profile.m
@@ -1,5 +1,52 @@
-% usage
+%% usage
 % visualize a transverse profile supporting multi-slice plot
+
+% load example
+load 'data.mat';
+sliced_data = slice_dist(data,'Emin','Emax',10);
+projected_data = slice_dist(data,'Emin','Emax',1);
+saveplotlycredentials('g.ballsaii','x4Ix3c17tD6m6S9zhr16')
+%%
+x = fx(projected_data,1,1);
+y = fx(projected_data,1,3);
+
+% matlab structure
+mainfig = figure;
+mainfig.Position = [50,100,1000,600];
+
+% subplot 1 
+sub1 = subplot(2,2,1);
+p1 = histogram2(fx(projected_data,1,1),fx(projected_data,1,3));
+c1 = colorbar;
+set(p1, 'BinMethod','fd','FaceColor','flat','EdgeColor','none');
+colormap(sub1,linspecer(128));
+view(2);
+
+% subplot 2
+sub2 = subplot(2,2,2);
+p2 = histogram(fx(projected_data,1,1));
+hold on;
+p3 = histogram(fx(projected_data,1,3));
+set([p2,p3],'EdgeColor','none','DisplayStyle','stairs')
+set(p3,'LineStyle','--');
+hold on;
+fakelegend = zeros(2, 1);
+fakelegendcolor = linspecer(2);
+fakelegend(1) = plot(NaN,NaN,'LineStyle','-','Color',fakelegendcolor(1,:));
+fakelegend(2) = plot(NaN,NaN,'LineStyle','--','Color',fakelegendcolor(2,:));
+fakelg = legend(fakelegend,'horizontal','vertical');
+set(fakelg,'Box','off','Location','best')
+
+
+
+
+
+
+
+function output = fx(sliced_data,i,j)
+output = sliced_data{i}(:,j);
+end
+%%
 
 
 
