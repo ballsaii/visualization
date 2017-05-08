8274c506ec8c3f034463cfc6ec6b4853e77bbbda
ballsaii <g.ballsaii@gmail.com>
2017-05-08 14:28:15 +0700

    complete transverse_profile
    - label centroids
    - fit dist by 'kernal' to get centroid

--- getcentroid.m
+++ getcentroid.m
@@ -1,17 +1,39 @@
-function [ x, y ] = getcentroid( hist )
+function loc = getcentroid(x,N,linsty,color)
 % usage 
+% getcentroid(data,bin)
 
-% create x and y
-x = (hist.BinEdges(1:end-1) + hist.BinWidth);
-y = hist.Values;
+% set xlimit to fitting curve
+xli = get(gca,'xlim');
+xPlot = linspace(xli(1),xli(2),N);
 
-[x,y] = prepareCurveData(x,y);
+% fit distribution
+pd = fitdist(x,'kernel','kernel','normal','support','unbounded');
+pdf_yPlot = pdf(pd,xPlot);
 
-% Set up fittype and options.
-ft = fittype( 'smoothingspline' );
+% find area of histogram to convert pdf to count
+[count,edge] = histcounts(x,N);
 
-% Fit model to data.
-[fitresult, gof] = fit(x,y,ft,'Normalize','on');
-plot(fitresult)
+center = edge+(edge(2)-edge(1))/2;
+center(end) = [];
+area = trapz(center,count);
+
+% unnormalize
+yPlot = pdf_yPlot * area;
+
+% plot in the previuos axes
+hold on;
+
+plot(xPlot,yPlot,'Color',color,'LineStyle',linsty)
+
+% find peak and its location
+[pk,loc] = findpeaks(yPlot,xPlot);
+
+
+% plot peak
+plot(loc,pk*1.01,'MarkerFaceColor',color,'Marker','v','MarkerEdgeColor','none')
+
+% label peak
+labecentroid = sprintf('centroid = %5.3f',loc);
+text(loc+0.10,pk*1.01,labecentroid,'Color',color);
 end
 
--- transverse_profile.m
+++ transverse_profile.m
@@ -1,6 +1,6 @@
 %% usage
 % visualize a transverse profile supporting multi-slice plot
-
+close all;clear;clc
 % load example
 load 'data.mat';
 sliced_data = slice_dist(data,'Emin','Emax',10);
@@ -11,38 +11,74 @@ y = fx(projected_data,1,3);
 
 % matlab structure
 mainfig = figure;
-mainfig.Position = [50,100,1000,400];
+mainfig.Position = [50,100,1500,400];
 
 % subplot 1 
-sub1 = subplot(1,2,1);
+sub1 = subplot(1,3,1);
 p1 = histogram2(fx(projected_data,1,1),fx(projected_data,1,3));
 c1 = colorbar;
 set(p1, 'BinMethod','scott','FaceColor','flat','EdgeColor','none');
 colormap(sub1,linspecer(128));
 view(2);
 
-% subplot 2
-sub2 = subplot(1,2,2);
-p2 = histogram(fx(projected_data,1,1));
+% subplot 2 for horizontal
+sub2 = subplot(1,3,2);
+p2 = histogram(fx(projected_data,1,1),'visible','on');
+fittingstyle = '-.';
+fittingcolor = 'red';
 hold on;
-p3 = histogram(fx(projected_data,1,3));
-set([p2,p3],'EdgeColor','none','DisplayStyle','stairs','BinMethod','scott')
-set(p3,'LineStyle','--');
-hold on;
-fakelegend = zeros(2, 1);
-fakelegendcolor = linspecer(2);
-fakelegend(1) = plot(NaN,NaN,'LineStyle','-','Color',fakelegendcolor(1,:));
-fakelegend(2) = plot(NaN,NaN,'LineStyle','--','Color',fakelegendcolor(2,:));
-fakelg = legend(fakelegend,'horizontal','vertical');
-set(fakelg,'Box','off','Location','best')
+% add fake legend
 
-% fit centroid
-[xc,yc] = getcentroid(p2)
+fakelegend(1) = plot(NaN,NaN,'LineStyle',fittingstyle,'Color',fittingcolor);
+
+% fit centroid horizontal
+loc_h = getcentroid(fx(projected_data,1,1),p2.NumBins,fittingstyle,fittingcolor);
+%fakelg1 = legend(sub2,fakelegend(1),'horizontal dist.');
+
+% add legend
+lg1 = legend(sub2,[p2,fakelegend(1)],'horizontal dist','fitting dist');
+hold off;
+
+% subplot 3 for vertical
+sub3 = subplot(1,3,3);
+p3 = histogram(fx(projected_data,1,3),'visible','on');
+fittingstyle = '-.';
+fittingcolor = 'red';
+hold on;
+% add fake legend
+fakelegend(2) = plot(NaN,NaN,'LineStyle',fittingstyle,'Color',fittingcolor);
+
+% fit centroid horizontal
+loc_v = getcentroid(fx(projected_data,1,3),p3.NumBins,fittingstyle,fittingcolor);
+%fakelg1 = legend(sub2,fakelegend(1),'horizontal dist.');
+
+% add legend
+lg2 = legend(sub3,[p3,fakelegend(2)],'vertical dist','fitting dist');
+
+hold off;
+set([lg1,lg2],'Location','southeast','Orientation','horizontal');
+set([p2,p3],'EdgeColor','black','EdgeAlpha',0.1,'DisplayStyle','bar','BinMethod','scott','LineWidth',0.5,'visible','on');
+
+set(sub3,'ylim',get(sub2,'ylim'));
+set([sub2,sub3],'XTick',get(sub1,'XTick'));
+
+% add units to axes
+xunit = 'horizontal position (mm)';
+yunit = 'vertical position (mm)';
+histunit = 'count';
+sub1.XLabel.String = xunit;
+sub1.YLabel.String = yunit;
+
+sub2.XLabel.String = xunit;
+sub2.YLabel.String = histunit;
+
+sub3.XLabel.String = yunit;
+sub3.YLabel.String = histunit;
 
 
 % centroid_fit = fitdist()
 function output = fx(sliced_data,i,j)
-output = sliced_data{i}(:,j);
+output = sliced_data{i}(:,j)*10;
 end
 %%
 
