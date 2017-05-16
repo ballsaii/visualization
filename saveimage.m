af8a7ae21ca25c2d8ee328ee15b1c0df57ec3efb
ballsaii <g.ballsaii@gmail.com>
2017-05-16 16:54:57 +0700

    complete phase space plot
    - more decor

--- saveimage.m
+++ saveimage.m
@@ -1,9 +1,7 @@
-function saveimage(h,path,file)
+function saveimage(f,path,file)
 % usage save each axes to eps file
-
-f = figure;
-ax = axes;
-new_handle = copyobj(h,ax);
-% pathfile = fullfile(path,file);
-% print(h,pathfile,'-dpng','-r600')
+for i=1:length(f)
+pathfile = fullfile(path,strjoin({file,num2str(i)},'_'));
+print(f(i),pathfile,'-dpng','-r600')
+end
 end
